fd9db775947341d2883d88f1f740fdb0db0fa86b
caizhongang <cai.zhongang@gmail.com>
2020-05-21 23:03:43 +0800

    feat: add NLZ filter

--- converter.py
+++ converter.py
@@ -9,8 +9,11 @@ from os.path import join, isdir
 import argparse
 from glob import glob
 
-from waymo_open_dataset.utils.frame_utils import parse_range_image_and_camera_projection, convert_range_image_to_point_cloud
+from waymo_open_dataset.utils.frame_utils import parse_range_image_and_camera_projection
 from waymo_open_dataset import dataset_pb2 as open_dataset
+from waymo_open_dataset import dataset_pb2
+from waymo_open_dataset.utils import range_image_utils
+from waymo_open_dataset.utils import transform_utils
 
 # Abbreviations:
 # WOD: Waymo Open Dataset
@@ -22,6 +25,11 @@ from waymo_open_dataset import dataset_pb2 as open_dataset
 # This switch, when set False, filters these boxes
 # It is safe to filter these boxes because they are not counted towards evaluation anyway
 filter_empty_3dboxes = False
+
+
+# There is no bounding box annotations in the No Label Zone (NLZ)
+# if set True, points in the NLZ are filtered
+filter_no_label_zone_points = True
 
 
 # Only bounding boxes of certain classes are converted
@@ -43,6 +51,7 @@ selected_waymo_locations = None
 
 # DATA_PATH = '/media/alex/Seagate Expansion Drive/waymo_open_dataset/domain_adaptation_training_labelled(partial)'
 # KITTI_PATH = '/home/alex/github/waymo_to_kitti_converter/tools/pose'
+
 
 class WaymoToKITTI(object):
 
@@ -234,7 +243,7 @@ class WaymoToKITTI(object):
                 :return:
                 """
         range_images, camera_projections, range_image_top_pose = parse_range_image_and_camera_projection(frame)
-        points_0, cp_points_0 = convert_range_image_to_point_cloud(
+        points_0, cp_points_0 = self.convert_range_image_to_point_cloud(
             frame,
             range_images,
             camera_projections,
@@ -245,7 +254,7 @@ class WaymoToKITTI(object):
         points_0 = np.concatenate(points_0, axis=0)
         intensity_0 = np.concatenate(intensity_0, axis=0)
 
-        points_1, cp_points_1 = convert_range_image_to_point_cloud(
+        points_1, cp_points_1 = self.convert_range_image_to_point_cloud(
             frame,
             range_images,
             camera_projections,
@@ -445,6 +454,91 @@ class WaymoToKITTI(object):
             for i in range(5):
                 if not isdir(d + str(i)):
                     os.makedirs(d + str(i))
+
+    def convert_range_image_to_point_cloud(frame,
+                                           range_images,
+                                           camera_projections,
+                                           range_image_top_pose,
+                                           ri_index=0):
+        """Convert range images to point cloud.
+        Args:
+          frame: open dataset frame
+           range_images: A dict of {laser_name, [range_image_first_return,
+             range_image_second_return]}.
+           camera_projections: A dict of {laser_name,
+             [camera_projection_from_first_return,
+             camera_projection_from_second_return]}.
+          range_image_top_pose: range image pixel pose for top lidar.
+          ri_index: 0 for the first return, 1 for the second return.
+        Returns:
+          points: {[N, 3]} list of 3d lidar points of length 5 (number of lidars).
+          cp_points: {[N, 6]} list of camera projections of length 5
+            (number of lidars).
+        """
+        calibrations = sorted(frame.context.laser_calibrations, key=lambda c: c.name)
+        points = []
+        cp_points = []
+
+        frame_pose = tf.convert_to_tensor(
+            value=np.reshape(np.array(frame.pose.transform), [4, 4]))
+        # [H, W, 6]
+        range_image_top_pose_tensor = tf.reshape(
+            tf.convert_to_tensor(value=range_image_top_pose.data),
+            range_image_top_pose.shape.dims)
+        # [H, W, 3, 3]
+        range_image_top_pose_tensor_rotation = transform_utils.get_rotation_matrix(
+            range_image_top_pose_tensor[..., 0], range_image_top_pose_tensor[..., 1],
+            range_image_top_pose_tensor[..., 2])
+        range_image_top_pose_tensor_translation = range_image_top_pose_tensor[..., 3:]
+        range_image_top_pose_tensor = transform_utils.get_transform(
+            range_image_top_pose_tensor_rotation,
+            range_image_top_pose_tensor_translation)
+        for c in calibrations:
+            range_image = range_images[c.name][ri_index]
+            if len(c.beam_inclinations) == 0:  # pylint: disable=g-explicit-length-test
+                beam_inclinations = range_image_utils.compute_inclination(
+                    tf.constant([c.beam_inclination_min, c.beam_inclination_max]),
+                    height=range_image.shape.dims[0])
+            else:
+                beam_inclinations = tf.constant(c.beam_inclinations)
+
+            beam_inclinations = tf.reverse(beam_inclinations, axis=[-1])
+            extrinsic = np.reshape(np.array(c.extrinsic.transform), [4, 4])
+
+            range_image_tensor = tf.reshape(
+                tf.convert_to_tensor(value=range_image.data), range_image.shape.dims)
+            pixel_pose_local = None
+            frame_pose_local = None
+            if c.name == dataset_pb2.LaserName.TOP:
+                pixel_pose_local = range_image_top_pose_tensor
+                pixel_pose_local = tf.expand_dims(pixel_pose_local, axis=0)
+                frame_pose_local = tf.expand_dims(frame_pose, axis=0)
+            range_image_mask = range_image_tensor[..., 0] > 0
+
+            # No Label Zone
+            if filter_no_label_zone_points:
+                nlz_mask = range_image_tensor[..., 0] != 1.0  # 1.0: in NLZ
+                range_image_mask = range_image_mask & nlz_mask
+
+            range_image_cartesian = range_image_utils.extract_point_cloud_from_range_image(
+                tf.expand_dims(range_image_tensor[..., 0], axis=0),
+                tf.expand_dims(extrinsic, axis=0),
+                tf.expand_dims(tf.convert_to_tensor(value=beam_inclinations), axis=0),
+                pixel_pose=pixel_pose_local,
+                frame_pose=frame_pose_local)
+
+            range_image_cartesian = tf.squeeze(range_image_cartesian, axis=0)
+            points_tensor = tf.gather_nd(range_image_cartesian,
+                                         tf.compat.v1.where(range_image_mask))
+
+            cp = camera_projections[c.name][ri_index]
+            cp_tensor = tf.reshape(tf.convert_to_tensor(value=cp.data), cp.shape.dims)
+            cp_points_tensor = tf.gather_nd(cp_tensor,
+                                            tf.compat.v1.where(range_image_mask))
+            points.append(points_tensor.numpy())
+            cp_points.append(cp_points_tensor.numpy())
+
+        return points, cp_points
 
 
     def get_intensity(self, frame, range_images, ri_index=0):
