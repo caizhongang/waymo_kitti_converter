2f4abe13a15d14e9f8aefb6f705da3282b44c059
caizhongang <cai.zhongang@gmail.com>
2020-09-22 15:28:51 +0800

    fix: remove pdb

--- tools/visual_point_cloud.py
+++ tools/visual_point_cloud.py
@@ -1,11 +1,10 @@
 import open3d as o3d
 import numpy as np
 
-pc_load_pathname = '/home/caizhongang/playground/kitti/test_crop_car/007401-000.bin'
+pc_load_pathname = '/home/caizhongang/github/waymo_kitti_converter/007283-000.bin'
 pc = np.fromfile(pc_load_pathname, dtype=np.float32).reshape(-1, 3)
 
 pcd = o3d.geometry.PointCloud() 
-import pdb; pdb.set_trace()
 pcd.points = o3d.utility.Vector3dVector(pc) 
 axis = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0,0,0]) 
 visual = [pcd, axis] 
